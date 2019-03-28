637ce8db86d124689ff264d785149a5398196344
陈鹏杰 <260083304@qq.com>
2019-03-28 11:05:13 +0800

    任务列表

---
+++ assets/js/highlight.js
@@ -0,0 +1,7 @@
+const hljs =require('highlight.js');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+});
