8a20d9785eeaa1b9c91042be97fc447836adc6f8
陈鹏杰 <260083304@qq.com>
2019-03-28 15:11:47 +0800

    任务列表

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // 载入electron模块
 const {app,BrowserWindow,Menu,shell,globalShortcut,ipcMain } = require("electron");
 
+let {buildMenu,findReopenMenuItem} = require('./demo/js/任务列表');
+
+
 // 创建应用程序对象
 // 创建一个浏览器窗口，主要用来加载HTML页面
 
@@ -21,7 +24,6 @@ app.on("window-all-closed",function(){
     // 判断当前操作系统是否是window系统，因为这个事件只作用在window系统中
     if(process.platform!="darwin"){
         // 退出整个应用程序
-        app.quit();
     }
 });
 
@@ -44,13 +46,13 @@ app.on('ondragstart', (event, filePath) => {
 app.on('browser-window-created', function () {
     let reopenMenuItem = findReopenMenuItem();
     if (reopenMenuItem) reopenMenuItem.enabled = false
-})
+});
 
 app.on('window-all-closed', function () {
     let reopenMenuItem = findReopenMenuItem();
-    if (reopenMenuItem) reopenMenuItem.enabled = true
+    if (reopenMenuItem) reopenMenuItem.enabled = true;
     app.quit()
-})
+});
 
 
 //定义一个创建浏览器窗口的方法
@@ -78,182 +80,8 @@ function createWindow(){
     });
 }
 
-/**
- *  自定义菜单
- */
-function buildMenu() {
-    let template = [
-        {
-            label: 'Edit ( 操作 )',
-            submenu: [{
-                label: 'Copy ( 复制 )',
-                accelerator: 'CmdOrCtrl+C',
-                role: 'copy'
-            }, {
-                label: 'Paste ( 粘贴 )',
-                accelerator: 'CmdOrCtrl+V',
-                role: 'paste'
-            }, {
-                label: 'Reload ( 重新加载 )',
-                accelerator: 'CmdOrCtrl+R',
-                click: function (item, focusedWindow) {
-                    if (focusedWindow) {
-                        // on reload, start fresh and close any old
-                        // open secondary windows
-                        if (focusedWindow.id === 1) {
-                            BrowserWindow.getAllWindows().forEach(function (win) {
-                                if (win.id > 1) {
-                                    win.close()
-                                }
-                            })
-                        }
-                        focusedWindow.reload()
-                    }
-                }
-            }]
-        },
-        {
-            label: 'Window ( 窗口 )',
-            role: 'window',
-            submenu: [{
-                label: 'Minimize ( 最小化 )',
-                accelerator: 'CmdOrCtrl+M',
-                role: 'minimize'
-            }, {
-                label: 'Close ( 关闭 )',
-                accelerator: 'CmdOrCtrl+W',
-                role: 'close'
-            }, {
-                label: '切换开发者工具',
-                accelerator: (function () {
-                    if (process.platform === 'darwin') {
-                        return 'Alt+Command+I'
-                    } else {
-                        return 'Ctrl+Shift+I'
-                    }
-                })(),
-                click: function (item, focusedWindow) {
-                    if (focusedWindow) {
-                        focusedWindow.toggleDevTools()
-                    }
-                }
-            }, {
-                type: 'separator'
-            }]
-        },
-        {
-            label: 'Help ( 帮助 ) ',
-            role: 'help',
-            submenu: [{
-                label: 'FeedBack ( 意见反馈 )',
-                click: function () {
-                    shell.openExternal('https://forum.iptchain.net')
-                }
-            },{
-                label:"测试二级测试",
-                submenu:[{
-                    label:"打开窗口",
-                    click(){
-                        shell.openExternal('https://idcpj.github.io');
-                    }
-                }]
-            }
-            ]
-        },
-    ];
-
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu) // 设置菜单部分
-
-    /**
-     * 增加更新相关的菜单选项
-     */
-    function addUpdateMenuItems (items, position) {
-        if (process.mas) return;
-
-        const version = app.getVersion();
-        let updateItems = [{
-            label: `Version ${version}`,
-            enabled: false
-        }, {
-            label: 'Checking for Update',
-            enabled: false,
-            key: 'checkingForUpdate'
-        }, {
-            label: 'Check for Update',
-            visible: false,
-            key: 'checkForUpdate',
-            click: function () {
-                require('electron').autoUpdater.checkForUpdates()
-            }
-        }, {
-            label: 'Restart and Install Update',
-            enabled: true,
-            visible: false,
-            key: 'restartToUpdate',
-            click: function () {
-                require('electron').autoUpdater.quitAndInstall()
-            }
-        }]
-
-        items.splice.apply(items, [position, 0].concat(updateItems))
-    }
 
 
-
-// 针对Mac端的一些配置
-    if (process.platform === 'darwin') {
-        const name = electron.app.getName();
-        template.unshift({
-            label: name,
-            submenu: [{
-                label: 'Quit ( 退出 )',
-                accelerator: 'Command+Q',
-                click: function () {
-                    app.quit()
-                }
-            }]
-        })
-
-        // Window menu.
-        template[3].submenu.push({
-            type: 'separator'
-        }, {
-            label: 'Bring All to Front',
-            role: 'front'
-        })
-
-        addUpdateMenuItems(template[0].submenu, 1)
-    }
-
-// 针对Windows端的一些配置
-    if (process.platform === 'win32') {
-        const helpMenu = template[template.length - 1].submenu;
-        addUpdateMenuItems(helpMenu, 0)
-    }
-
-}
-
-/**
- * 自定义菜单
- * @returns {*}
- */
-function findReopenMenuItem () {
-    const menu = Menu.getApplicationMenu();
-    if (!menu) return;
-
-    let reopenMenuItem
-    menu.items.forEach(function (item) {
-        if (item.submenu) {
-            item.submenu.items.forEach(function (item) {
-                if (item.key === 'reopenMenuItem') {
-                    reopenMenuItem = item
-                }
-            })
-        }
-    })
-    return reopenMenuItem;
-}
 
 
 /**
@@ -262,7 +90,7 @@ function findReopenMenuItem () {
 function shortcut() {
     const ret = globalShortcut.register('CommandOrControl+9', () => {
         console.log('CommandOrControl+X is pressed')
-    })
+    });
     if (!ret) {
         console.log('registration failed')
     }
@@ -283,4 +111,4 @@ app.on('will-quit', () => {
 ipcMain.on("main_liston",(event,arg)=>{
     console.log(arg);
     event.sender.send("renderer_liston",'main to renderer')
-})
+});
